935f789c18aa30a7ffc31d57d58d571a3d5d4d30
jacobdlewis <jacobdlewis@gmail.com>
2015-02-02 13:26:42 -0600

    AJAX response is returning data

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-(function(){
-'use strict'
+/* global $: false*/
+  var url,
+      $stockName,
+      $stockQty,
+      AJAXresponse,
+      $buyStocks=$('buyStocks');
 
-var url,
-    $stockName,
-    $stockQty,
-    data,
-    $buyStocks=$('buyStocks');
+  $('#buyStocks').click(function(){
+    'use strict'
+    $stockName = $('#stockName').val().toUpperCase();
+    $stockQty = $('#stockQty').val();
+    console.log($stockName);
+    console.log($stockQty);
+    url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + $stockName + '&callback=?';
 
-$buyStocks.change(function(){
-  $stockName = $('#stockName').val();
-  $stockQty = $('stockQty').val();
-  url = "http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=" + stockName + "&callback=?";
-
-  $.getJSON(url, function() {
-  data = this.response;
-  stockName = response.Name;
+    $.getJSON(url, function(data) {
+      AJAXresponse = data;
+      console.log(data);
     });
 
-  });
-});
+    });
 
 
 
 
 
-})();
+
