31e9b160ea082d45ec36f97e97860e92ca506391
jacobdlewis <jacobdlewis@gmail.com>
2015-02-02 14:20:25 -0600

    total adjusts correctly when stocks are added

--- main.js
+++ main.js
@@ -3,15 +3,16 @@
       $stockName,
       $stockQty,
       AJAXresponse,
+      total = 0,
       $buyStocks=$('buyStocks');
 
   $('#buyStocks').click(function(){
-    'use strict'
     $stockName = $('#stockName').val().toUpperCase();
     $stockQty = $('#stockQty').val();
     console.log($stockName);
     console.log($stockQty);
     url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + $stockName + '&callback=?';
+
 
     $.getJSON(url, function(data) {
       AJAXresponse = data;
@@ -20,10 +21,13 @@
       $($tempRow).append('<td>' + data.Name + '</td>');
       $($tempRow).append('<td>' + data.LastPrice + '</td>');
       $($tempRow).append('<td>' + $stockQty + '</td>');
-      $($tempRow).append('<td>' + data.Change + '</td>');
-      $($tempRow).append('<td>' + data.ChangePercent + '</td>');
+      $($tempRow).append('<td>' + data.Change.toFixed(2) + '</td>');
+      $($tempRow).append('<td>' + data.ChangePercent.toFixed(2) + '</td>');
       $($tempRow).append('<button id="RemoveRow">Remove</button>');
       $('table').append($tempRow);
+      $('#tempTotal').remove();
+      total += (data.LastPrice * $stockQty);
+      $('table').append('<p id="tempTotal">$' + total + '</p>');
     });
 
     });
