647d0761f556743f6df57d741769b09c72b5d98f
jacobdlewis <jacobdlewis@gmail.com>
2015-02-03 12:28:57 -0600

    remove button removes rows, but totals don't work correctly

--- main.js
+++ main.js
@@ -8,18 +8,20 @@
       total = 0,
       $buyStocks=$('buyStocks');
 
-  $('#buyStocks').click(function(){
+  $('#buyStocks').on('click', function(){
+
     $stockName = $('#stockName').val().toUpperCase();
     $stockQty = $('#stockQty').val();
-    console.log($stockName);
-    console.log($stockQty);
     url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + $stockName + '&callback=?';
-
 
     $.getJSON(url, function(data) {
       AJAXresponse = data;
       console.log(data);
       var $tempRow = $('<tr></tr>');
+      $($tempRow).attr("data-name", data.Name);
+      $($tempRow).attr("data-price", data.LastPrice);
+      $($tempRow).attr("data-qty", $stockQty);
+      $($tempRow).attr("data-change", data.Change.toFixed(2));
       $($tempRow).append('<td>' + data.Name + '</td>');
       $($tempRow).append('<td class = "price">' + data.LastPrice + '</td>');
       $($tempRow).append('<td class = "qty">' + $stockQty + '</td>');
@@ -33,6 +35,16 @@
     });
 
    $('.table').on('click', '.RemoveRow', function(){
+    var tempPrice = $(this).closest('tr').attr('data-price');
+    console.log($(this).closest('tr').attr('data-price'));
+    var tempQty = $(this).closest('tr').attr('data-qty');
+    console.log($(this).closest('tr').attr('data-qty'));
+    var tempTotal = (tempPrice * tempQty);
+    console.log($('#tempTotal').val());
+    total -= tempTotal;
+    tempTotal = 0;
+    $('#tempTotal').remove();
+    $('#portfolioTotal').append('<p id="tempTotal">$' + total + '</p>');
     $(this).closest('tr').remove();
   });
 
